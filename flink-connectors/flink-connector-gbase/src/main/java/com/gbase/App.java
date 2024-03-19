0f115b06f222a16288ef418d69f2b008ba38aa6d
fanweiming <fanweiming@gbase.cn>
2024-03-19 22:47:29 +0800

    connector gbase init

---
+++ flink-connectors/flink-connector-gbase/src/main/java/com/gbase/App.java
@@ -0,0 +1,13 @@
+package com.gbase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
